f5f8931b4fa8556864cc8842ec6e2f997784b4fd
Adhitya R <adhityarangaraj@gmail.com>
2026-04-13 21:48:30 +0530

    Added error handling in backend

--- location.py
+++ location.py
@@ -6,8 +6,9 @@ geolocator = Nominatim(user_agent="my_location")
 def locate(city):
     location_name = city
     location = geolocator.geocode(location_name)
-
+    print(location)
     if location:
         return [location.latitude, location.longitude]
     else:
         return [0,0]
+locate("Chennai")
